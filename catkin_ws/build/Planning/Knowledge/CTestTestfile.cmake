d8ac1221f9cc4f7386ba98d331dc16843ff36fe4
Festino <becamume@gmail.com>
2023-08-31 16:07:03 -0600

    add model miztli rviz

---
+++ catkin_ws/build/Planning/Knowledge/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/festino/Miztli_doc_2.0/catkin_ws/src/Planning/Knowledge
+# Build directory: /home/festino/Miztli_doc_2.0/catkin_ws/build/Planning/Knowledge
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
